bde4f8796aa7c7358c3eca083170dab52a4684dd
Sobakiin <wgreen16@my.bcit.ca>
2021-05-05 09:55:28 -0700

    Built a basic script for the web application, will improve and document later.

---
+++ controller/recycle_controller.js
@@ -0,0 +1,12 @@
+
+
+let recycleController = {
+    search: (req,res) => {
+        res.render("recycle/index")
+    },
+    results: (req,res)=> {
+        res.render("recycle/result")
+    }
+}
+
+module.exports = recycleController;
