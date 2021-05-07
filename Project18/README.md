559550825ff743e691fead3ba169ee46c461c42d
AlexG-A01234719 <aguo11@my.bcit.ca>
2021-05-06 23:16:26 -0700

    Remove duplicated directory

--- Project18/README.md
+++
@@ -1,8 +0,0 @@
-﻿# Project18
-
- Start by running the command:
- "npm install"
-
- If that does not work, then install npm through this link:
-
- https://www.npmjs.com/package/dotenv
--- Project18/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const app = require("./app");
-const databaseAccess = require("./databaseAccess")
-
-test("it should return water bottle from database", async () => {
-    const response = databaseAccess.searchDatabase();
-    expect(response).toEqual({name: "waterbottle"})
-})
-
