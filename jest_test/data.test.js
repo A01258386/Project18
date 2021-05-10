49067f19c28196e5345d8d0a38e1c675ef3ad68f
Alex Hong <honguttdc@gmail.com>
2021-05-10 15:22:32 -0700

    switch from main branch to develop branch

---
+++ jest_test/data.test.js
@@ -0,0 +1,20 @@
+//get data database
+const item = require('../db/dbquery.js');
+// const keyword = 'asd';
+
+// console.log(item);
+
+// test keyword 'milk carton'
+test('getting result with keyword \'milk carton\'', () => {
+    expect(item.some(({ ItemName }) => ItemName === 'milk carton')).toBe(true);
+});
+
+//test when user keyword not found
+test('getting result with keyword \'milk carton\'', () => {
+    expect(item.some(({ ItemName }) => ItemName === 'napalm bomb')).not.toBe(true);
+});
+
+// //test the keyword not contain special character
+// test('expecting keyword not found', () => {
+//     expect(keyword).not.toMatch(/[\t\r\n]|(--[^\r\n]*)|(\/\*[\w\W]*?(?=\*)\*\/)/gi);
+// });
