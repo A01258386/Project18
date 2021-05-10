fec8a6f36fcf690df72f3e9af60a2ede916d6c3e
Alex Hong <honguttdc@gmail.com>
2021-05-10 15:09:58 -0700

    create test functions for testing database

--- jest_test/data.test.js
+++ jest_test/data.test.js
@@ -1,23 +1,20 @@
 //get data database
 const item = require('../db/dbquery.js');
+// const keyword = 'asd';
 
-console.log(item);
+// console.log(item);
 
+// test keyword 'milk carton'
 test('getting result with keyword \'milk carton\'', () => {
-    expect(item).toEqual(expect.arrayContaining(expect.objectContaining({ ItemName: "milk carton" })));
+    expect(item.some(({ ItemName }) => ItemName === 'milk carton')).toBe(true);
 });
 
-// // test keyword 'milk jug'
-// test('getting result with keyword \'milk jug\'', () => {
-//     expect(item.ItemName).toBe("milk carton");
-// });
-
-// //test when user keyword not found
-// test('expecting keyword not found', () => {
-//     expect(item.ItemName).not.toBe("naphan bomb");
-// });
+//test when user keyword not found
+test('getting result with keyword \'milk carton\'', () => {
+    expect(item.some(({ ItemName }) => ItemName === 'napalm bomb')).not.toBe(true);
+});
 
 // //test the keyword not contain special character
 // test('expecting keyword not found', () => {
-//     expect(item.ItemName).not.toMatch(/[\t\r\n]|(--[^\r\n]*)|(\/\*[\w\W]*?(?=\*)\*\/)/gi);
+//     expect(keyword).not.toMatch(/[\t\r\n]|(--[^\r\n]*)|(\/\*[\w\W]*?(?=\*)\*\/)/gi);
 // });
