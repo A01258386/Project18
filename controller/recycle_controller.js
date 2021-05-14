3133eeffb3f1b3a1397cd7faeb9288ed5b201455
Alex Hong <honguttdc@gmail.com>
2021-05-13 19:21:39 -0700

    finish getting user input and redirect user to result page

--- controller/recycle_controller.js
+++ controller/recycle_controller.js
@@ -1,13 +1,4 @@
 const database = require('../db/dbquery');
-// const keyword = require('../index.js');
-
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-let keyword = app.post('/results', function (req, res){
-    res.send(req.body.textarea);
-});
 
 let recycleController = {
     search: (req,res) => {
@@ -17,8 +8,8 @@ let recycleController = {
         res.render("recycle/index")
     },
     results: (req, res) => {
-        let items = database.compare_keyword("Item", "can");
-        console.log(keyword)
+        let keyword = req.body.textarea;
+        let items = database.compare_keyword("Item", keyword);
         res.render("recycle/result", { items, keyword });
     }
 }
